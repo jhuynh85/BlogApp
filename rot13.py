210f037bbd60aa14ddb4f1f512980bd9be7d6971
Joseph Huynh <joe.huynh@gmail.com>
2012-04-26 02:01:36 -0700

    RO13 page complete

--- rot13.py
+++ rot13.py
@@ -1,4 +1,5 @@
-# import webapp2
+import webapp2
+from escape import *
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
@@ -21,46 +22,29 @@ def rot13(str):
         s += d
     return s
 
-form="""
-<form method="post">
-    What is your birthday?
-    <br>
-    
-    <label> Month
-        <input type="text" name="month" value="%(month)s">
-    </label>
-    
-    <label> Day
-        <input type="text" name="day" value="%(day)s">
-    </label>
-    
-    <label> Year
-        <input type="text" name="year" value="%(year)s">
-    </label>
-    <div style="color: red">%(error)s</div>
-    <br>
-    <br>
-    <input type="submit">
-</form>
+page="""
+<html>
+    <head>
+        <title>Unit 2 Rot 13</title>
+    </head>
+
+    <body>
+        <h2>Enter some text to ROT13:</h2>
+        <form method="post">
+            <textarea name="text" style="height: 100px; width: 400px;">%s</textarea>
+            <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
 """
 
-"""
+
 class ROT13Handler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write(form)
+        self.response.out.write(page % "")
 
     def post(self):
-        user_month = self.request.get('month')
-        user_day = self.request.get('day')
-        user_year = self.request.get('year')
-        
-        month = valid_month(user_month)
-        day = valid_day(user_day)
-        year = valid_year(user_year)
-        
-        if not (month and day and year):
-            self.write_form("That doesn't look valid to me, friend.",
-            user_month, user_day, user_year)
-        else:
-            self.redirect("/thanks")
-"""
+        t = self.request.get('text')
+        t = escape_html(rot13(t))
+        self.response.out.write(page % t)
